46edc1fc0f2a3071ecdffef6b389bc1ccd7fba04
landon-sturgeon <sturgeon.landon@gmail.com>
2019-09-01 15:04:41 -0700

    added child js scripts

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { TodoBanner } from "./TodoBanner";
+import { TodoCreater } from "./TodoCreater";
+import { TodoRow } from "./TodoRow";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: "Landon",
+            todoItems: [{action: "Read", done: false},
+                        {action: "Study", done: false},
+                        {action: "Call Parents", done: false},
+                        {action: "Go for a jog", done: false}],
+        }
+    }
+
+    updateNewTextValue = (event) => {
+        this.setState({newItemText: event.target.value});
+    }
+
+    createNewTodo = (task) => {
+        if (!this.state.todoItems
+                  .find(item => item.action === task)) {
+                this.setState({
+                    todoItems: [...this.state.todoItems, {action: task, done: false}],
+                });
+           }
+    }
+
+    toggleTodo = (todo) =>
+        this.setState({ todoItems:
+            this.state.todoItems.map(item => item.action === todo.action ? {...item, done: !item.done } : item)
+        });
+
+    todoTableRows = () =>
+        this.state.todoItems.map(item =>
+            <TodoRow key={ item.action } item={ item } callback={ this.toggleTodo }/>
+        );
+
+    render = () =>
+        <div>
+            <TodoBanner name={this.state.userName} tasks={ this.state.todoItems } />
+            <div className="container-fluid">
+                <TodoCreater callback={ this.createNewTodo } />
+                    <table className="table table-striped table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Description</th>
+                                <th>Done</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { this.todoTableRows() }
+                        </tbody>
+                    </table>
+            </div>
+        </div>
 }
-
-export default App;
