6555d8733c498f5f4de48b37bbb4d14258bc9eb3
Landon Sturgeon <Landon@Jennifers-MacBook-Air.local>
2019-09-02 15:30:18 -0700

    added primer app, cleaned up todo App

---
+++ primer/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+export default class App extends Component {
+    render = () =>
+    <h4 className="bg-primary text-white text-center p-2 m-1">
+        Component Element
+    </h4>
+}
