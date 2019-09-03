e5f124ea156db0cfcece5715d9059e83d5d26905
landon-sturgeon <sturgeon.landon@gmail.com>
2019-09-02 17:23:52 -0700

    added form example to App

--- primer/src/App.js
+++ primer/src/App.js
@@ -23,22 +23,38 @@ export default class App extends Component {
 
     render = () => {
         return (
-            <div className="container-fluid p-4">
-                <div className="row bg-info text-white p-2">
-                    <div className="col font-weight-bold">Value</div>
-                    <div className="col-6 font-weight-bold">Even?</div>
+            // Example of table implementation
+//            <table className="table table-striped table-bordered table-sm">
+//                <thead className="bg-info text-white">
+//                    <tr><th>Value</th><th>Even?</th></tr>
+//                </thead>
+//                <tbody>
+//                    <tr>
+//                        <td>{ this.state.count }</td>
+//                        <td>{ this.isEven(this.state.count) }</td>
+//                    </tr>
+//                </tbody>
+//                <tfoot className="text-center">
+//                    <tr>
+//                        <td colSpan="2">
+//                            <button className="btn btn-info m-2"
+//                                onClick={ this.handleClick }>
+//                                Click Me
+//                            </button>
+//                        </td>
+//                    </tr>
+//                </tfoot>
+//            </table>
+
+            // Example of form implementation
+            <div className="m-2">
+                <div className="form-group">
+                    <label>Name:</label>
+                    <input className="form-control" />
                 </div>
-                <div className="row bg-light p-2 border">
-                    <div className="col">{ this.state.count }</div>
-                    <div className="col-6">{ this.isEven( this.state.count ) }</div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <button className="btn btn-info m-2"
-                            onClick={ this.handleClick }>
-                            Click Me
-                        </button>
-                    </div>
+                <div className="form-group">
+                    <label>City:</label>
+                    <input className="form-control" />
                 </div>
             </div>
         )
