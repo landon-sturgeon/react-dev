bbbce2015641b9d57b64751aeb83e52e819eaa92
Landon Sturgeon <sturgeon.landon@gmail.com>
2019-09-05 14:20:39 -0700

    added ToggleLink component

--- sportsstore/src/shop/CategoryNavigation.js
+++ sportsstore/src/shop/CategoryNavigation.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { ToggleLink } from "../ToggleLink";
 
 export class CategoryNavigation extends Component {
 
     render () {
         return <React.Fragment>
-            <Link className="btn btn-secondary btn-block"
-                to={ this.props.baseUrl }>All</Link>
+            <ToggleLink to={ this.props.baseUrl } exact={ true }>All</ToggleLink>
             { this.props.categories && this.props.categories.map( cat =>
-                <Link className="btn btn-secondary btn-block" key={ cat }
+                <ToggleLink key={ cat }
                     to={ `${this.props.baseUrl}/${cat.toLowerCase()}`}>
                     { cat }
-                </Link>
+                </ToggleLink>
             )}
         </React.Fragment>
     }
