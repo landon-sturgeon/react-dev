e5c114005542f5862084594c8e59e44044b10671
Landon Sturgeon <sturgeon.landon@gmail.com>
2019-09-07 20:08:09 -0600

    added graphql for admin data edits

--- sportsstore/server.js
+++ sportsstore/server.js
@@ -2,10 +2,18 @@ const express = require("express");
 const jsonServer = require("json-server");
 const chokidar = require("chokidar");
 const cors = require("cors");
+
+const fs = require("fs");
+const { buildSchema } = require("graphql");
+const graphqlHTTP = require("express-graphql");
+const queryResolvers = require("./serverQueriesResolver");
+const mutationResolvers = require("./serverMutationsResolver");
+
 const fileName = process.argv[2] || "./data.js";
 const port = process.argv[3] || 3500;
 
 let router = undefined;
+let graph = undefined;
 
 const app = express();
 
@@ -15,6 +23,13 @@ const createServer = () => {
         router = jsonServer.router(fileName.endsWith(".js")
             ? require(fileName)() : fileName
         );
+        let schema = fs.readFileSync("./serverQueriesSchema.graphql", "utf-8")
+            + fs.readFileSync("./serverMutationsSchema.graphql", "utf-8");
+        let resolvers = { ...queryResolvers, ...mutationResolvers };
+        graph = graphqlHTTP({
+            schema: buildSchema(schema), rootValue: resolvers,
+            graphiql: true, context: { db: router.db }
+        })
     }, 100)
 }
 
@@ -23,6 +38,7 @@ createServer();
 app.use(cors());
 app.use(jsonServer.bodyParser);
 app.use("/api", (req, resp, next) => router(req,resp, next));
+app.use("/graphql", (req, resp, next) => graph(req, resp, next));
 
 chokidar.watch(fileName).on("change", () => {
     console.log("Reloading web service data...");
