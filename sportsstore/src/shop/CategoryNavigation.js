58ea796ddaf9d771511db7210821efcaf37d32f9
Landon Sturgeon <sturgeon.landon@gmail.com>
2019-09-09 09:28:14 -0600

    added link to admin page from category page

--- sportsstore/src/shop/CategoryNavigation.js
+++ sportsstore/src/shop/CategoryNavigation.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { ToggleLink } from "../ToggleLink";
+import { Link } from "react-router-dom";
 
 export class CategoryNavigation extends Component {
 
@@ -16,6 +17,10 @@ export class CategoryNavigation extends Component {
                         { cat }
                     </ToggleLink>
                 )}
+                <Link className="btn btn-block btn-secondary fixed-bottom m-2 col-3"
+                    to="/admin">
+                    Administration
+                </Link>
             </React.Fragment>
         )
     }
