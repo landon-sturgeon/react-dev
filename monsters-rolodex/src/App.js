fffb4874032c267f95e35bd3fd806f047285e4c6
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-14 20:01:58 -0700

    added custom app component and fetch

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor () {
+    super();
+
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello Landon</p>
-          <button>Change Text</button>
-        </header>
+        {
+          this.state.monsters.map(monster =>
+            <h1 key={monster.id}>{ monster.name }</h1>
+          )
+        }
       </div>
     );
   }
