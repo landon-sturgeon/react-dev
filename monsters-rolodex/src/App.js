976d103ab717ce0d5d4824746f5a61e302627e37
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-15 07:20:47 -0700

    added monster card-list and cards

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { CardList } from "./components/card-list/card-list.component.js"
 import './App.css';
 
 class App extends Component {
@@ -20,11 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster =>
-            <h1 key={monster.id}>{ monster.name }</h1>
-          )
-        }
+        <CardList monsters={ this.state.monsters }></CardList>
       </div>
     );
   }
