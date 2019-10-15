f98d8dbfad854af1663c4ab746ebd36004de4946
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-15 09:06:32 -0700

    added search bar, index css

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { CardList } from "./components/card-list/card-list.component.js"
+import { CardList } from "./components/card-list/card-list.component"
+import { SearchBar } from "./components/search-bar/search-bar.component"
 import './App.css';
 
 class App extends Component {
@@ -8,7 +9,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
@@ -18,10 +20,24 @@ class App extends Component {
     .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  }
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+        monster.name.toLowerCase().includes(searchField.toLowerCase())
+      );
+
     return (
       <div className="App">
-        <CardList monsters={ this.state.monsters }></CardList>
+        <h1>Monsters Rolodex</h1> 
+        <SearchBar
+          placeholder="search monsters"
+          handleChange={ this.handleChange }
+        />
+        <CardList monsters={ filteredMonsters }></CardList>
       </div>
     );
   }
--- monsters-rolodex/src/components/card/card.component.js
+++ monsters-rolodex/src/components/card/card.component.js
@@ -3,7 +3,7 @@ import "./card.styles.css";
 
 export const Card = (props) => (
   <div className="card-container">
-    <img alt="monster" src={ `https://robohash.org/${ props.monster.id }?set=set2&size=180x180`} />
+    <img alt="monster" src={ `https://robohash.org/${ props.monster.id }?set=set1&size=180x180`} />
     <h2> {props.monster.name}</h2>
     <p> {props.monster.email} </p>
   </div>
