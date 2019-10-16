902889bffa61c0700899149804c161844e014461
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-16 15:01:01 -0700

    update to redirect home if user signed in

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 import './App.css';
@@ -15,9 +15,9 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-const { setCurrentUser } = this.props;
+    const { setCurrentUser } = this.props;
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
@@ -42,17 +42,31 @@ const { setCurrentUser } = this.props;
       <div>
         <Header />
         <Switch>
-          <Route exact path="/" component={ HomePage } />
-          <Route path="/shop" component={ ShopPage } />
-          <Route path="/sign-in" component= { SignInAndSignUpPage } />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route 
+            exact 
+            path="/sign-in" 
+            render={() => 
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SignInAndSignUpPage />
+              )
+            } 
+          />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
