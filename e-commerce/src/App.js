6baa2b3dcd3435d5b235c603e86d2750db9760e5
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-15 20:19:07 -0700

    added collection previews and collection item components

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -3,19 +3,15 @@ import './App.css';
 import { Route, Switch } from "react-router-dom";
 
 import HomePage from "../src/pages/homepage/homepage.component";
+import ShopPage from "../src/pages/shop/shop.component";
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={ HomePage } />
-        <Route path="/shop/hats" component={ HatsPage} />
+        <Route path="/shop" component={ ShopPage } />
       </Switch>
     </div>
   );
