963c71344d1a1e7f6cf3cf1588588ff873e7736b
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-15 19:42:23 -0700

    added links to separate pages

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from "react-router-dom";
 
 import HomePage from "../src/pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={ HomePage } />
+        <Route path="/shop/hats" component={ HatsPage} />
+      </Switch>
     </div>
   );
 }
