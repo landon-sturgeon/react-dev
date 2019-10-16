098397caca41aee2ae43fe4a4f4f488e627f6d5c
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-16 13:04:48 -0700

    finished signin and signup methods

--- e-commerce/src/components/sign-in/sign-in.component.js
+++ e-commerce/src/components/sign-in/sign-in.component.js
@@ -4,7 +4,7 @@ import "./sign-in.styles.scss";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
     constructor(props) {
@@ -16,14 +16,21 @@ class SignIn extends React.Component {
         }
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: "", password: ""})
-    }
+        const { email, password } = this.state;
+
+        try{
+            await auth.signInWithEmailAndPassword(email, password);
+            this.setState({ email: "", password: "" });
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     handleChange = event => {
         const { value, name } = event.target;
-        this.setState({ [name]: value})
+        this.setState({ [name]: value});
     }
 
     render () {
--- e-commerce/src/components/sign-up/sign-up.component.js
+++ e-commerce/src/components/sign-up/sign-up.component.js
@@ -1,11 +1,10 @@
 import React from "react";
 
 import FormInput from "../form-input/form-input.component";
-import customButton from "../custom-button/custom-button.component";
+import CustomButton from "../custom-button/custom-button.component";
 import { auth, createUserProfileDocument } from "../../firebase/firebase.utils";
 
 import "./sign-up.styles.scss";
-import CustomButton from "../custom-button/custom-button.component";
 
 class SignUp extends React.Component {
     constructor() {
