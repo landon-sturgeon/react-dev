6fd7e0a007c00765d0005c88a1f5155644fe3ae6
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-16 07:34:23 -0700

    added header component and svg

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -4,11 +4,13 @@ import { Route, Switch } from "react-router-dom";
 
 import HomePage from "../src/pages/homepage/homepage.component";
 import ShopPage from "../src/pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={ HomePage } />
         <Route path="/shop" component={ ShopPage } />
