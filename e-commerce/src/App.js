d74726ab0c8c50babd90457fe4f28be8e9fbaa74
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-16 12:34:32 -0700

    added sign up form and styling

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -6,7 +6,7 @@ import HomePage from "../src/pages/homepage/homepage.component";
 import ShopPage from "../src/pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "../src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
 
@@ -21,9 +21,22 @@ class App extends React.Component {
   unsubscribeFromAuth = null
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+    });
   }
 
   componentWillUnmount() {
