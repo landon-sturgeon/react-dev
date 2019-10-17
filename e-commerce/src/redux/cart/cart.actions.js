1bee467287ee8fb7651f7b2d37befe0299bcaf76
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-17 08:14:41 -0700

    added item increment/decrement to checkout page

--- e-commerce/src/redux/cart/cart.actions.js
+++ e-commerce/src/redux/cart/cart.actions.js
@@ -14,3 +14,8 @@ export const removeItemFromCart = item => ({
     type: CartActionTypes.REMOVE_ITEM_FROM_CART,
     payload: item
 });
+
+export const removeItem = item => ({
+    type: CartActionTypes.REMOVE_ITEM,
+    payload: item
+});
