4444cf20b7ce37aad1bb9dae6012b82d2bfb6be0
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-17 13:14:30 -0700

    removed some modules

--- e-commerce/src/redux/cart/cart.types.js
+++
@@ -1,8 +0,0 @@
-const CartActionTypes = {
-    TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
-    ADD_ITEM: "ADD_ITEM",
-    REMOVE_ITEM_FROM_CART: "REMOVE_ITEM_FROM_CART",
-    REMOVE_ITEM: "REMOVE_ITEM"
-}
-
-export default CartActionTypes;
--- e-commerce/src/redux/user/user.selector.js
+++
@@ -1,8 +0,0 @@
-import { createSelector } from "reselect";
-
-const selecUser = state => state.user;
-
-export const selectCurrentUser = createSelector (
-    [selecUser],
-    (user) => user.currentUser
-);
