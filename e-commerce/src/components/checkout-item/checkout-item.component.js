fa569f580e23210bb7614020439b02c3a919fbcc
landon-sturgeon <sturgeon.landon@gmail.com>
2019-10-17 07:39:29 -0700

    added remove item from cart feature

--- e-commerce/src/components/checkout-item/checkout-item.component.js
+++ e-commerce/src/components/checkout-item/checkout-item.component.js
@@ -1,17 +1,29 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import { removeItemFromCart } from "../../redux/cart/cart.actions";
 
 import "./checkout-item.styles.scss";
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity }}) => (
-    <div className="checkout-item">
-        <div className="image-container">
-            <img src={ imageUrl } alt="item" />
+const CheckoutItem = ({ cartItem, clearItem }) => {
+    const { name, imageUrl, price, quantity } = cartItem;
+    return (
+        <div className="checkout-item">
+            <div className="image-container">
+                <img src={ imageUrl } alt="item" />
+            </div>
+            <span className="name">{ name }</span>
+            <span className="quantity">{ quantity }</span>
+            <span className="price">{ price }</span>
+            <div className="remove" onClick={() => clearItem(cartItem)}>
+                &#10005;
+            </div>
         </div>
-        <span className="name">{ name }</span>
-        <span className="quantity">{ quantity }</span>
-        <span className="price">{ price }</span>
-        <div className="remove">&#10005;</div>
-    </div>
-);
+    )
+};
 
-export default CheckoutItem;
+const mapDispathToProps = dispatch => ({
+    clearItem: item => dispatch(removeItemFromCart(item))
+});
+
+export default connect(null, mapDispathToProps)(CheckoutItem);
--- e-commerce/src/redux/cart/cart.reducer.js
+++ e-commerce/src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from "./cart.types";
-import { addItemToCart} from "./cart.utils";
+import { addItemToCart } from "./cart.utils";
 
 const INITIAL_STATE = {
     hidden: true,
@@ -17,6 +17,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload)
+            };
+        case CartActionTypes.REMOVE_ITEM_FROM_CART:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(
+                    cartItem => cartItem.id !== action.payload.id
+                )
             };
         default:
             return state;
